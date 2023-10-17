2b70fb39f9bbc3fea6de3a1512961960a4a73d77
rahmanazizf <rahmanazizf@gmail.com>
2023-10-17 10:57:10 +0700

    add get order by id

--- go-rest-api/routers/routers.go
+++ go-rest-api/routers/routers.go
@@ -10,5 +10,6 @@ func StartServer() *gin.Engine {
 	router := gin.Default()
 	router.POST("/orders", controllers.CreateOrder)
 	router.GET("/orders", controllers.GetAllOrders)
+	router.GET("/orders/:orderID", controllers.GetOrderByID)
 	return router
 }
