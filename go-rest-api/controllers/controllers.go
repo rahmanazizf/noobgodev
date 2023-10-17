1cadd168c4c396b394ca5b72bfcd18762fe25c2f
rahmanazizf <rahmanazizf@gmail.com>
2023-10-17 10:52:21 +0700

    add get orders controller

--- go-rest-api/controllers/controllers.go
+++ go-rest-api/controllers/controllers.go
@@ -1,8 +1,10 @@
 package controllers
 
 import (
+	"fmt"
 	"godev/go-rest-api/models"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -22,6 +24,36 @@ func CreateOrder(ctx *gin.Context) {
 
 	ctx.JSON(http.StatusOK, gin.H{
 		"data":    newOrder,
+		"message": "success",
+	})
+}
+
+var found = false
+var orderData *models.Order
+
+func GetOrderByID(ctx *gin.Context) {
+	id := ctx.Param("orderID")
+	for _, o := range OrderData {
+		if id, _ := strconv.Atoi(id); o.OrderID == id {
+			found = true
+			orderData = &o
+		}
+	}
+	if !found {
+		ctx.AbortWithStatusJSON(http.StatusNotFound, gin.H{
+			"data":    orderData,
+			"message": fmt.Sprintf("Order ID %s is not available", id),
+		})
+	}
+	ctx.JSON(http.StatusOK, gin.H{
+		"data":    orderData,
+		"message": "sucess",
+	})
+}
+
+func GetAllOrders(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"data":    OrderData,
 		"message": "success",
 	})
 }
