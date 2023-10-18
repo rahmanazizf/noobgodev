0d7e956adbad0707b73f625b03c9fc97941a4c7b
rahmanazizf <rahmanazizf@gmail.com>
2023-10-18 09:10:27 +0700

    add controller for update order karena sementara belum disambungkan ke database (data hanya disimpan sementara) jadi dibuat fungsi tambahan  untuk menghapus data berdasarkan order id

--- go-rest-api/controllers/controllers.go
+++ go-rest-api/controllers/controllers.go
@@ -57,3 +57,48 @@ func GetAllOrders(ctx *gin.Context) {
 		"message": "success",
 	})
 }
+
+var existingOrder *models.Order
+
+func UpdateOrder(ctx *gin.Context) {
+	id := ctx.Param("orderID")
+	var updatedOrder *models.Order
+	ctx.ShouldBindJSON(&updatedOrder)
+	for _, o := range OrderData {
+		if id, _ := strconv.Atoi(id); o.OrderID == id {
+			found = true
+			existingOrder = &o
+		}
+	}
+	if !found {
+		ctx.AbortWithStatusJSON(http.StatusNotFound, gin.H{
+			"data":    orderData,
+			"message": fmt.Sprintf("Order ID %s is not available", id),
+		})
+	}
+
+	updatedOrder.OrderID = existingOrder.OrderID
+	OrderData = append(RemoveIndex(OrderData, updatedOrder.OrderID), *updatedOrder)
+	ctx.JSON(http.StatusOK, gin.H{
+		"data":    updatedOrder,
+		"message": "update success",
+	})
+
+}
+
+var iStop int
+
+func RemoveIndex(slc []models.Order, orderID int) []models.Order {
+	newOrderData := []models.Order{}
+	for i, o := range slc {
+		if o.OrderID == orderID {
+			iStop = i
+			break
+		}
+		newOrderData = append(newOrderData, o)
+	}
+	if iStop+1 >= len(slc) {
+		return append(newOrderData, slc[iStop+1:]...)
+	}
+	return newOrderData
+}
