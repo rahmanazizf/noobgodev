f553cc4eb93fd2a7ab7d563e8459835e06d24f61
rahmanazizf <rahmanazizf@gmail.com>
2023-10-18 10:54:31 +0700

    tidy up main

--- go-rest-api/main.go
+++ go-rest-api/main.go
@@ -5,14 +5,5 @@ import (
 )
 
 func main() {
-	// r := gin.Default()
-
-	// r.GET("/", func(ctx *gin.Context) {
-	// 	ctx.JSON(200, gin.H{
-	// 		"message": "Hello, World!",
-	// 	})
-	// })
-
-	// r.Run(":8280")
 	routers.StartServer().Run(":8382")
 }
