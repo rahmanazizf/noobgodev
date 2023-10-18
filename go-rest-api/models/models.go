1b9d6ce11a9a12db92eba0d04cd7ea129bb729c0
rahmanazizf <rahmanazizf@gmail.com>
2023-10-18 13:46:17 +0700

    update models

--- go-rest-api/models/models.go
+++ go-rest-api/models/models.go
@@ -5,18 +5,18 @@ import "time"
 type Order struct {
 	OrderID      int       `gorm:"primary_key" json:"id"`
 	CustomerName string    `json:"customerName"`
-	CreatedAt    time.Time `json:"createdAt"`
-	UpdatedAt    time.Time `json:"updatedAt"`
+	CreatedAt    time.Time `json:"-"`
+	UpdatedAt    time.Time `json:"-"`
 	OrderedAt    time.Time `json:"orderedAt"`
 	Items        []Item    `json:"items"`
 }
 
 type Item struct {
-	ItemID      int       `gorm:"primary_key" json:"id"`
+	ItemID      int       `gorm:"primary_key" json:"-"`
 	ItemName    string    `json:"name"`
 	Description string    `json:"description"`
 	Quantity    int       `json:"quantity"`
-	OrderID     int       `json:"order_id"`
-	CreatedAt   time.Time `json:"createdAt"`
-	UpdatedAt   time.Time `json:"updatedAt"`
+	OrderID     uint      `json:"-"`
+	CreatedAt   time.Time `json:"-"`
+	UpdatedAt   time.Time `json:"-"`
 }
