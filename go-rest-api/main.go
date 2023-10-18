a2f1f29065c22944df388661ed4d0999574877b3
rahmanazizf <rahmanazizf@gmail.com>
2023-10-18 15:37:14 +0700

    establish connection in main

--- go-rest-api/main.go
+++ go-rest-api/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"godev/go-rest-api/connection"
+	"godev/go-rest-api/controllers"
 	"godev/go-rest-api/routers"
 )
 
 func main() {
+	conn := connection.DBConnection()
+	db := controllers.NewConnection(conn)
+	controllers.EstablishConnection(db)
 	routers.StartServer().Run(":8382")
 }
